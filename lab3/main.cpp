982c31ffa16823d24a86592cfb4c55ef0c63e58f
Kirill Efimenko <vivichv9@yandex.ru>
2023-10-26 12:46:26 +0300

    add octagon

--- lab3/main.cpp
+++ lab3/main.cpp
@@ -3,6 +3,7 @@
 #include "include/figures/Triangle.hpp"
 #include "include/figures/Square.hpp"
 #include "include/figures/Octagon.hpp"
+#include "include/containers/Vector.hpp"
 #include <vector>
   
 int main() {
@@ -14,7 +15,19 @@ int main() {
     Square s(Point(1, 1), Point(1, 3), Point(3, 3), Point(3, 1));
     std::cout << s.calculate_centre() << std::endl;
     std::cout << (double) s << std::endl;
-    
+
+    Point p1(10, 10);
+    Point p2(p1.get_x() - (1 + std::sqrt(2)), p1.get_y() + 1);
+    Point p3(p2.get_x() - 1, p2.get_y() + 1 + std::sqrt(2));
+    Point p4(p3.get_x() + 1, p3.get_y() + (1 + std::sqrt(2)));
+    Point p5(p4.get_x() + 1 + std::sqrt(2), p4.get_y() + 1);
+    Point p6(p5.get_x() + 1 + std::sqrt(2), p5.get_y() - 1);
+    Point p7(p6.get_x() + 1, p6.get_y() - (1 + std::sqrt(2)));
+    Point p8(p7.get_x() - 1, p7.get_y() - (1 + std::sqrt(2)));
+
+    Octagon o(p1, p2, p3, p4, p5, p6, p7, p8);
+    std::cout << o.calculate_centre() << std::endl;
+    std::cout << (double) o << std::endl;
   } catch (std::invalid_argument& e) {
     std::cout << e.what() << std::endl;
   }
--- lab3/src/figures/Octagon.cpp
+++ lab3/src/figures/Octagon.cpp
@@ -10,9 +10,56 @@ bool Octagon::validate(
     const Point& p7,
     const Point& p8
   ) noexcept {
+    
+  double sum_x = p1.get_x() + p2.get_x() + p3.get_x() + p4.get_x() + p5.get_x() + p6.get_x() + p7.get_x() + p8.get_x();
+  double sum_y = p1.get_y() + p2.get_y() + p3.get_y() + p4.get_y() + p5.get_y() + p6.get_y() + p7.get_y() + p8.get_y();
+  Point centre(sum_x / 8.0, sum_y / 8.0);
 
-  // TODO
-  return true;
+  double inaccuracy = 1e-10;
+
+  // straight lines from center to corner
+  double len_to_p1 = Point::line_len(centre, p1);
+  double len_to_p2 = Point::line_len(centre, p2);
+  double len_to_p3 = Point::line_len(centre, p3);
+  double len_to_p4 = Point::line_len(centre, p4);
+  double len_to_p5 = Point::line_len(centre, p5);
+  double len_to_p6 = Point::line_len(centre, p6);
+  double len_to_p7 = Point::line_len(centre, p7);
+  double len_to_p8 = Point::line_len(centre, p8);
+
+  if (
+    std::abs(len_to_p1 - len_to_p2) < inaccuracy &&
+    std::abs(len_to_p2 - len_to_p3) < inaccuracy &&
+    std::abs(len_to_p3 - len_to_p4) < inaccuracy &&
+    std::abs(len_to_p4 - len_to_p5) < inaccuracy && 
+    std::abs(len_to_p5 - len_to_p6) < inaccuracy &&
+    std::abs(len_to_p6 - len_to_p7) < inaccuracy &&
+    std::abs(len_to_p7 - len_to_p8) < inaccuracy
+  ) {
+    return true;
+  }
+
+  return false;
+}
+
+Octagon::Octagon(
+  const Point& p1,
+  const Point& p2,
+  const Point& p3,
+  const Point& p4,
+  const Point& p5,
+  const Point& p6,
+  const Point& p7,
+  const Point& p8
+  ) {
+  
+  bool is_octagon = validate(p1, p2, p3, p4, p5, p6, p7, p8);
+
+  if (!is_octagon) {
+    throw std::invalid_argument("Invalid points. Octagon can not created!");
+  }
+
+  coordinates = {p1, p2, p3, p4, p5, p6, p7, p8};
 }
 
 Point Octagon::calculate_centre() const {
@@ -24,10 +71,11 @@ Point Octagon::calculate_centre() const {
     centre_y += coordinates[i].get_y();
   }
 
-  return Point(centre_x / 8, centre_y / 8);
+  return Point(centre_x / 8.0, centre_y / 8.0);
 }
 
 Octagon::operator double() const {
-  // TODO
-  return 1;
+  double side_len = Point::line_len(coordinates[0], coordinates[1]);
+
+  return 2 * std::pow(side_len, 2) * (1 + std::sqrt(2));
 }
