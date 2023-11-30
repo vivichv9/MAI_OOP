864ed609c00c7a7978badb2725681aff7e8a1b80
Kirill Efimenko <vivichv9@yandex.ru>
2023-11-30 23:20:02 +0300

    hui

--- lab5/main.cpp
+++ lab5/main.cpp
@@ -4,6 +4,7 @@
 #include "src/allocator.cpp"
 #include <list.h>
 #include "src/list.cpp"
+#include <chrono>
 
 int main() {
   std::map<int, int, std::less<int>, mystd::allocator<int, 1000>> m;
@@ -16,6 +17,7 @@ int main() {
     std::cout << '{' << e.first << ", " << e.second << "}, ";
   }
 
+  //SLAWA BEBROW AND MORGENSHTERN
   std::cout << std::endl;
   mystd::list<int, mystd::allocator<int, 1000>> lst;
   mystd::list<int, mystd::allocator<int, 1000>> a;
@@ -28,5 +30,16 @@ int main() {
   std::cout << a.front() << std::endl;
   std::cout << lst.max_size() << std::endl;
   std::cout << lst.front() << std::endl;
+
+
+  auto start = std::chrono::steady_clock::now();
+  mystd::list<size_t, mystd::allocator<size_t, 1000000000>> lsta;
+  for (size_t i = 0; i < 100000000; ++i) {
+    lsta.push_front(i);
+  }
+  auto end = std::chrono::steady_clock::now();
+  auto time = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+  std::cout << time << std::endl;
+
   return 0;
 }
